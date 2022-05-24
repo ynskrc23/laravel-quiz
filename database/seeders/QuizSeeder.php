4445bdd4b9f901567569b65dc539e7533f9504b4
Yunus Karaca <ynskrc82@gmail.com>
2022-05-24 23:23:37 +0300

    seed, factory yapısı oluşturuldu. quiz migration oluşturuldu.

--- database/seeders/QuizSeeder.php
+++ database/seeders/QuizSeeder.php
@@ -2,19 +2,19 @@
 
 namespace Database\Seeders;
 
+use App\Models\Quiz;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+
+class QuizSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            QuizSeeder::class
-        ]);
+        Quiz::factory(10)->create();
     }
 }
