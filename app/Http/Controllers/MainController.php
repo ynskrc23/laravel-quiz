e2ddb968abe3e42009ebd69f36e5a24553e22692
Yunus Karaca <ynskrc82@gmail.com>
2022-06-19 16:16:10 +0300

    quiz sonuç görüntüleme sayfası yapıldı

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -16,7 +16,12 @@ class MainController extends Controller
     }
 
     public function quiz($slug){
-        $quiz = Quiz::whereSlug($slug)->with('questions')->first();
+        $quiz = Quiz::whereSlug($slug)->with('questions.my_answer')->first() ?? abort(404,'Quiz bulunamadı');
+
+        if($quiz->my_result)
+        {
+            return view('quiz_result', compact('quiz'));
+        }
         return view('quiz', compact('quiz'));
     }
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
         @livewireStyles
 
         <!-- Scripts -->
